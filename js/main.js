e1d2bdcb109c746e369ad041852104789f3689ad
Soniateixeira <soniahistoria2013@gmail.com>
2023-05-16 14:47:31 -0300

    primeira versao

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-alert("Deu certo")
+var botao = document.querySelector('#botaoheader')
+botao.addEventListener("click", dizerMeuNome)
+
+function dizerMeuNome() {
+    alert("Prazer, me chamo Sônia!")
+}
+
+/*
+var: botao = document.querySelector('btn1')
+<button id = "btn1">Clique em mim</button>
+var: botao = document.querySelector("btn2")
+<button id = "btn2">Clique em mim também</button>
+*/
